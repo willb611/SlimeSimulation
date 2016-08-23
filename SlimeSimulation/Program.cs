adb105701d2fcf9bd338bf5df7d8f7f6777ad73f
William A. O. Brown <will.b611@gmail.com>
2016-08-23 12:53:35 +0100

    Remove useless comments

--- SlimeSimulation/Program.cs
+++ SlimeSimulation/Program.cs
@@ -11,12 +11,6 @@ namespace SlimeSimulation
     {
         static void Main(string[] args)
         {
-            //var flowCalculator = new FlowCalculator(new LupDecompositionSolver());
-            //SlimeNetworkGenerator slimeNetworkGenerator = new LatticeSlimeNetworkGenerator(13, 0.05);
-            //var initial = slimeNetworkGenerator.Generate();
-            //SimulationUpdater simulationUpdater = new SimulationUpdater(flowCalculator, new SlimeNetworkAdaptionCalculator(feedbackParameter));
-            //var controller = new SimulationController(flowAmount, simulationUpdater, initial);
-            //controller.RunSimulation();
             var applicationStarter = new ApplicationStartController();
             applicationStarter.Render();
         }
