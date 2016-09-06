c00ec10657419314a7484ac785df0227ed0d16d9
William A. O. Brown <will.b611@gmail.com>
2016-09-06 10:27:51 +0100

    Move component which configures lattice creation into a seperate class

---
+++ SlimeSimulation/View/WindowComponent/SimulationControlComponent/SimulationCreaterComponent/LatticeGenerationControlComponent.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gtk;
+using SlimeSimulation.Configuration;
+using SlimeSimulation.StdLibHelpers;
+
+namespace SlimeSimulation.View.WindowComponent.SimulationControlComponent.SimulationCreaterComponent
+{
+    public class LatticeGenerationControlComponent : VBox
+    {
+        private TextView _latticeGeneratorRowSizeTextView;
+        private TextView _latticeGeneratorProbabiltyOfNewFoodTextView;
+        private TextView _latticeGeneratorMinimumFoodSourcesTextView;
+
+        private List<string> errorComponents;
+        private readonly Dictionary<TextView, Label> _textViewLabelMapping = new Dictionary<TextView, Label>();
+
+        public LatticeGenerationControlComponent(LatticeGraphWithFoodSourcesGenerationConfig defaultConfig)
+        {
+            Add(RowSizeBox(defaultConfig));
+            Add(ProbabilityNewNodeIsFoodBox(defaultConfig));
+            Add(MinimumNumberFoodSourcesBox(defaultConfig));
+        }
+
+        private Widget MinimumNumberFoodSourcesBox(LatticeGraphWithFoodSourcesGenerationConfig defaultConfig)
+        {
+            Label description = new Label("Minimum number of food sources in the network");
+            TextView textView = new TextView();
+            textView.Buffer.Text = defaultConfig.MinimumFoodSources.ToString();
+            _latticeGeneratorMinimumFoodSourcesTextView = textView;
+            return MakeHbox(description, textView);
+        }
+
+        private Widget ProbabilityNewNodeIsFoodBox(LatticeGraphWithFoodSourcesGenerationConfig defaultConfig)
+        {
+            Label description = new Label("Probability new nodes in network are food sources");
+            TextView textView = new TextView();
+            textView.Buffer.Text = defaultConfig.ProbabilityNewNodeIsFoodSource.ToString();
+            _latticeGeneratorProbabiltyOfNewFoodTextView = textView;
+            return MakeHbox(description, textView);
+        }
+
+        private Widget RowSizeBox(LatticeGraphWithFoodSourcesGenerationConfig defaultConfig)
+        {
+            Label description = new Label("Number of rows in lattice to generate");
+            TextView textView = new TextView();
+            textView.Buffer.Text = defaultConfig.Size.ToString();
+            _latticeGeneratorRowSizeTextView = textView;
+            return MakeHbox(description, textView);
+        }
+
+        private Widget MakeHbox(Label description, TextView textView)
+        {
+            HBox hBox = new HBox();
+            hBox.Add(description);
+            hBox.Add(textView);
+            _textViewLabelMapping[textView] = description;
+            return hBox;
+        }
+
+        public LatticeGraphWithFoodSourcesGenerationConfig ReadGenerationConfig()
+        {
+            _textViewLabelMapping.Clear();
+            double? probabilityNewNodeIsFood = _latticeGeneratorProbabiltyOfNewFoodTextView.ExtractDoubleFromView();
+            AddToErrorsIfNull(probabilityNewNodeIsFood, _latticeGeneratorProbabiltyOfNewFoodTextView);
+            int? minFoodSources = _latticeGeneratorMinimumFoodSourcesTextView.ExtractIntFromView();
+            AddToErrorsIfNull(minFoodSources, _latticeGeneratorMinimumFoodSourcesTextView);
+            int? rowSize = _latticeGeneratorRowSizeTextView.ExtractIntFromView();
+            AddToErrorsIfNull(rowSize, _latticeGeneratorRowSizeTextView);
+
+            if (probabilityNewNodeIsFood.HasValue && minFoodSources.HasValue && rowSize.HasValue)
+            {
+                return new LatticeGraphWithFoodSourcesGenerationConfig(rowSize.Value,
+                    probabilityNewNodeIsFood.Value, minFoodSources.Value);
+            } else
+            {
+                return null;
+            }
+        }
+
+        private void AddToErrorsIfNull(double? nullableValue, TextView componentWhichReturnedValue)
+        {
+            if (nullableValue == null)
+            {
+                errorComponents.Add(_textViewLabelMapping[componentWhichReturnedValue].Text);
+            }
+        }
+
+        internal IEnumerable<string> ErrorMessages()
+        {
+            return errorComponents;
+        }
+    }
+}
--- SlimeSimulation/View/Windows/ApplicationStartWindow.cs
+++ SlimeSimulation/View/Windows/ApplicationStartWindow.cs
@@ -23,10 +23,6 @@ namespace SlimeSimulation.View.Windows
         private Button _beginSimulationButton;
         private CheckButton _shouldAllowDisconnectionCheckButton;
 
-        private TextView _latticeGeneratorRowSizeTextView;
-        private TextView _latticeGeneratorProbabiltyOfNewFoodTextView;
-        private TextView _latticeGeneratorMinimumFoodSourcesTextView;
-
         private TextView _feedbackParamTextView;
         private FlowAmountControlComponent _flowAmountControlComponent;
 
@@ -34,7 +30,7 @@ namespace SlimeSimulation.View.Windows
 
 
         private List<string> _errors = new List<string>();
-
+        private LatticeGenerationControlComponent _latticeGenerationControlComponent;
 
         public ApplicationStartWindow(string windowTitle, ApplicationStartWindowController windowController)
             : base(windowTitle, windowController)
@@ -48,7 +44,7 @@ namespace SlimeSimulation.View.Windows
             var container = MakeComponents();
             container.Add(_flowAmountControlComponent);
             container.Add(FeedbackParameterBox());
-            container.Add(MatrixGenerationProperties());
+            container.Add(_latticeGenerationControlComponent);
             container.Add(_shouldAllowDisconnectionCheckButton);
             container.Add(_beginSimulationButton);
             container.Add(ErrorDisplayComponent());
@@ -63,6 +59,8 @@ namespace SlimeSimulation.View.Windows
             _shouldAllowDisconnectionCheckButton = new ShouldAllowSlimeDisconnectionButton(_defaultConfig.ShouldAllowDisconnection);
             _beginSimulationButton = new BeginSimulationComponent();
             _errorLabel = new Label();
+            _latticeGenerationControlComponent =
+                new LatticeGenerationControlComponent(_defaultConfig.GenerationConfig);
             return new VBox();
         }
 
@@ -95,22 +93,24 @@ namespace SlimeSimulation.View.Windows
             HighglightErrorIfNull(feedbackParam, _feedbackParamTextView);
             double? flowAmount = _flowAmountControlComponent.TextView.ExtractDoubleFromView();
             HighglightErrorIfNull(flowAmount, _flowAmountControlComponent.TextView);
-            double? probabilityNewNodeIsFood = _latticeGeneratorProbabiltyOfNewFoodTextView.ExtractDoubleFromView();
-            HighglightErrorIfNull(probabilityNewNodeIsFood, _latticeGeneratorProbabiltyOfNewFoodTextView);
-            int? minFoodSources = _latticeGeneratorMinimumFoodSourcesTextView.ExtractIntFromView();
-            HighglightErrorIfNull(minFoodSources, _latticeGeneratorMinimumFoodSourcesTextView);
-            int? rowSize = _latticeGeneratorRowSizeTextView.ExtractIntFromView();
-            HighglightErrorIfNull(rowSize, _latticeGeneratorRowSizeTextView);
             bool shouldAllowDisconnection = _shouldAllowDisconnectionCheckButton.Active;
-            if (feedbackParam.HasValue && flowAmount.HasValue &&
-                probabilityNewNodeIsFood.HasValue && minFoodSources.HasValue && rowSize.HasValue)
+            if (feedbackParam.HasValue && flowAmount.HasValue)
             {
                 try
                 {
-                    var generationConfig = new LatticeGraphWithFoodSourcesGenerationConfig(rowSize.Value,
-                        probabilityNewNodeIsFood.Value, minFoodSources.Value);
-                    return new SimulationConfiguration(generationConfig, flowAmount.Value,
-                        new SlimeNetworkAdaptionCalculatorConfig(feedbackParam.Value), shouldAllowDisconnection);
+                    var generationConfig = _latticeGenerationControlComponent.ReadGenerationConfig();
+                    if (generationConfig == null)
+                    {
+                        foreach (var errorMessage in _latticeGenerationControlComponent.ErrorMessages())
+                        {
+                            _errors.Add(errorMessage);
+                        }
+                    }
+                    else
+                    {
+                        return new SimulationConfiguration(generationConfig, flowAmount.Value,
+                            new SlimeNetworkAdaptionCalculatorConfig(feedbackParam.Value), shouldAllowDisconnection);
+                    }
                 }
                 catch (ArgumentException e)
                 {
@@ -138,41 +138,6 @@ namespace SlimeSimulation.View.Windows
             }
         }
 
-        private Widget MatrixGenerationProperties()
-        {
-            VBox vBox = new VBox();
-            vBox.Add(RowSizeBox());
-            vBox.Add(ProbabilityNewNodeIsFoodBox());
-            vBox.Add(MinimumNumberFoodSourcesBox());
-            return vBox;
-        }
-
-        private Widget MinimumNumberFoodSourcesBox()
-        {
-            Label description = new Label("Minimum number of food sources in the network");
-            TextView textView = new TextView();
-            textView.Buffer.Text = _defaultConfig.GenerationConfig.MinimumFoodSources.ToString();
-            _latticeGeneratorMinimumFoodSourcesTextView = textView;
-            return HBox(description, textView);
-        }
-
-        private Widget ProbabilityNewNodeIsFoodBox()
-        {
-            Label description = new Label("Probability new nodes in network are food sources");
-            TextView textView = new TextView();
-            textView.Buffer.Text = _defaultConfig.GenerationConfig.ProbabilityNewNodeIsFoodSource.ToString();
-            _latticeGeneratorProbabiltyOfNewFoodTextView = textView;
-            return HBox(description, textView);
-        }
-
-        private Widget RowSizeBox()
-        {
-            Label description = new Label("Number of rows in lattice to generate");
-            TextView textView = new TextView();
-            textView.Buffer.Text = _defaultConfig.GenerationConfig.Size.ToString();
-            _latticeGeneratorRowSizeTextView = textView;
-            return HBox(description, textView);
-        }
 
         private Widget FeedbackParameterBox()
         {
