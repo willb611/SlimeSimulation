62936b69fe2c005284855ff0346cc32deb0dddb1
William A. O. Brown <will.b611@gmail.com>
2017-03-04 09:53:47 +0000

    Add simple test for GridGeneration

---
+++ SlimeSimulationTests/Model/Generation/GridGraphWithFoodSourcesGeneratorTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SlimeSimulation.Model.Generation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SlimeSimulation.Configuration;
+
+namespace SlimeSimulation.Model.Generation.Tests
+{
+    [TestClass()]
+    public class GridGraphWithFoodSourcesGeneratorTests
+    {
+        [TestMethod()]
+        public void Generate_SizeThree_ShouldWork()
+        {
+            for (int i = 3; i < 9; i++)
+            {
+                var generator = new GridGraphWithFoodSourcesGenerator(new LatticeGraphWithFoodSourcesGenerationConfig(i));
+                var network = generator.Generate();
+                Assert.IsTrue(network.FoodSources.Count >= 2, "2 food sources should always be produced");
+            }
+        }
+    }
+}
